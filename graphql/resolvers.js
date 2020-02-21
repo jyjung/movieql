08006df74a51c94cc54931606f801e4d98647809
Jung JinYoung <bungker@gmail.com>
2020-02-21 19:58:27 +0900

    0221 edit

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,6 @@
 // 수정시 graphql 과 resolvers 가 같이 수정되어야 한다.  
+
+/*
 const people = [
     {
         id: 1,
@@ -35,3 +37,39 @@ const resolvers = {
 }
 
 export default resolvers; 
+*/
+
+const movies = [
+    {
+        id: 1,
+        name: "Avatar",
+        socre: 99
+    }, 
+    {
+        id: 1,
+        name: "Avatar",
+        socre: 99
+    },
+    {
+        id: 1,
+        name: "Avatar",
+        socre: 99
+    }
+]; 
+
+export const getMovies = () => movies;   
+export const getById = id => {
+    const filteredMovies = movies.filter(movie => movie.id == id);
+    return filteredMovies[0]; 
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id != id); 
+    if(movies.length > cleanedMovies) {
+        movies = cleanedMovies; 
+        return true; 
+    } else {
+        return false; 
+    }
+}
+// 나의 첫번째 GraphQL서버 만들기 #8 Defining Mutations  2:46
